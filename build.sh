7053869b0cb6af9fffab98862c97e7cda20ffa49
Anders Janmyr <anders@janmyr.com>
2010-09-13 16:53:46 +0200

    removed minified file from git and build it into dist

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mkdir dist
 sprocketize -C src box2d.js > dist/box2d.js
-`java -jar vendor/yuicompressor/yuicompressor-2.4.2.jar --charset UTF8 dist/box2d.js -o box2d.min.js`
+`java -jar vendor/yuicompressor/yuicompressor-2.4.2.jar --charset UTF8 dist/box2d.js -o dist/box2d.min.js`
